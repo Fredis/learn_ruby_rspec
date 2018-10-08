24c9a0ab3e1cd7bdacc60d30fa11339b0037f0c7
Apelerin <arcanynka@gmail.com>
2018-10-08 17:04:10 +0200

    PIGGY juju

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,32 @@
 #write your code here
+
+def wordtrans(word)
+  if word[0] =~ /[aeiuoy]/ #si commence par voyelle
+    return word += "ay"
+  else
+    i = 0
+    cns = String.new
+    while /[aeiuoy]/.match?(word[i]) == false
+      cns += word[i]
+      i += 1
+    end
+    if cns[cns.size - 1] =~ /[qQ]/ && word[cns.size] =~ /[uU]/
+      cns += word[cns.size]
+    end
+    word.delete!(cns)
+    return word + cns + "ay"
+  end
+end
+
+def translate(str)
+  if str.split(/\W+/).size != 1
+    arr = str.split(/\W+/)
+    ret = String.new
+    arr.each do |value|
+      ret += wordtrans(value) + ' '
+    end
+    return ret.rstrip
+  else
+    return wordtrans(str)
+  end
+end
