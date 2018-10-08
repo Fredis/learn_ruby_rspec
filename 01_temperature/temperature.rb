ecc6c04158c0af5652b00ae9fae7e675f18f75b5
Fredis <fredericstojicevic@gmail.com>
2018-10-08 13:58:59 +0200

    The four first exercises.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,7 @@
-#write your code here
+def ftoc(temp) #Conversion des températures de Fahrenheit vers Celsius
+	return (temp-32.0)*5.0/9.0		
+end
+
+def ctof(temp) #Conversion des températures de Celsius vers Fahrenheit
+	return (temp*9.0/5.0)+32.0
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,41 @@
-#write your code here
+def add(first, second) #Méthode pour effectuer une addition
+	return first + second
+end
+
+def subtract(first, second) #Méthode pour effectuer une soustraction
+	return first - second
+end
+
+def sum(array) #Méthode pour effectuer la somme des valeurs dans un array
+	result = 0.0
+	array.each do |number|
+		result = result + number
+	end
+	return result
+end
+
+def multiply (first, second) #Méthode pour effectuer une multiplication
+	return first*second
+end
+
+def power(first, second) #Méthode pour calculer la puissance d'un nombre sur la base du second
+	return first**second
+end
+
+def factorial(number) #Méthode pour calculer la factorielle d'un nombre
+
+	if number == 0 #Condition pour gérer le nombre 0
+		return result = 1
+	end
+	
+	result = number
+	numberCache = number
+	number.times do
+		numberCache -= 1
+		if numberCache != 0
+			result = result*numberCache
+		end
+	end
+	return result
+
+end
