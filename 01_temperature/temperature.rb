b7f3a8b011761dc7051bf48f9e61df8fb1aa071f
Apelerin <arcanynka@gmail.com>
2018-10-08 14:56:04 +0200

    Opti

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,7 +1,7 @@
 def ftoc(temp) #Conversion des températures de Fahrenheit vers Celsius
-	return (temp-32.0)*5.0/9.0		
+	return (temp - 32.0 ) * 5.0 / 9.0		
 end
 
 def ctof(temp) #Conversion des températures de Celsius vers Fahrenheit
-	return (temp*9.0/5.0)+32.0
+	return (temp * 9.0 / 5.0) + 32.0
 end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -15,27 +15,14 @@ def sum(array) #Méthode pour effectuer la somme des valeurs dans un array
 end
 
 def multiply (first, second) #Méthode pour effectuer une multiplication
-	return first*second
+	return first * second
 end
 
 def power(first, second) #Méthode pour calculer la puissance d'un nombre sur la base du second
-	return first**second
+	return first ** second
 end
 
 def factorial(number) #Méthode pour calculer la factorielle d'un nombre
-
-	if number == 0 #Condition pour gérer le nombre 0
-		return result = 1
-	end
-	
-	result = number
-	numberCache = number
-	number.times do
-		numberCache -= 1
-		if numberCache != 0
-			result = result*numberCache
-		end
-	end
-	return result
-
+  return 1 if number == 1 || number == 0
+  return (number * factorial(number - 1))
 end
