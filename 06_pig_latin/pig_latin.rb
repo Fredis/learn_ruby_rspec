393e2171bef7ccff8effccecac974c52af81381b
Juliette Audema <juliette.audema@gmail.com>
2018-10-08 17:55:09 +0200

    Commentaires dernier exo

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,35 +1,31 @@
 def wordtrans(word)
-  if word[0] =~ /[aeiuoy]/ #si commence par voyelle
-    return word += "ay"
-  else
+  if word[0] =~ /[aeiuoy]/ #si le mot commence par une voyelle
+    return word += "ay" #ajout de "ay" à la fin du mot
+  else #si le mot commence par une consonne
     i = 0
-    cns = String.new
-    while /[aeiuoy]/.match?(word[i]) == false
-      cns += word[i]
+    consonnes = String.new
+    while /[aeiuoy]/.match?(word[i]) == false #cette boucle vérifie que les premières lettres du mot sont bien des consonnes (ie ne sont pas des voyelles) et s'arrête sinon. Elle ajoute toutes les consonnes dans la variable cosonnes.
+      consonnes += word[i]
       i += 1
     end
-    if cns[cns.size - 1] =~ /[qQ]/ && word[cns.size] =~ /[uU]/
-      cns += word[cns.size]
+    if consonnes[consonnes.size - 1] =~ /[qQ]/ && word[consonnes.size] =~ /[uU]/ #si la variable consonnes se termine par [qQ] ET est suivi par [uU]
+      consonnes += word[consonnes.size] #la variable consonnes récupère toutes les lettres jusqu'à [Uu]
     end
-    word.delete!(cns)
-    return word + cns + "ay"
+    word.delete!(consonnes) #on supprime du mot le premier bout de consonnes et dans le cas où ça s'applique la suite "[Qq][Uu]".
+    return word + consonnes + "ay" #on met la variable consonnes à la fin du mot et on ajoute "ay".
   end
 end
 
 def translate(str)
-  if str.split(/\W+/).size != 1
-    arr = str.split(/\W+/)
-    ret = String.new
+  if str.split(/\W+/).size != 1 #On détermine s'il y a plus d'un mot dans le string.
+    arr = str.split(/\W+/) #On stocke tous les mots dans un array.
+    ret = String.new 
     arr.each do |value|
-      ret += wordtrans(value) + ' '
+      ret += wordtrans(value) + ' ' #Pour chaque mot de l'array, on applique la méthode wordtrans au mot et on les met dans le string "ret", chaque fois séparés par un espace.
     end
-    return ret.rstrip
+    return ret.rstrip #Supprime le dernier espace du ret.
   else
-    return wordtrans(str)
+    return wordtrans(str) #S'il y a qu'un mot on applique la méthode wordtrans au mot.
   end
 end
 
-
-  # Test-driving bonus:
-  # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
-  # * retain the punctuation from the original phrase
